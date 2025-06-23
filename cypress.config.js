5fe0ce6f09d5c093ac9a9ef190a7a5e576596dc8
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-06-23 17:55:29 +0530

    First commit

---
+++ cypress.config.js
@@ -0,0 +1,40 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  video: true,
+  reporter: 'cypress-mochawesome-reporter', // for html reports 
+  //projectId: "b6bh2b",
+  //projectId: "m4t5cs",
+  reporterOptions: {
+   
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    enableCode:false,
+    embeddedvideos: true,
+    video: true,
+  },
+  e2e: {
+    baseUrl: "https://jplus.jainam.in",
+    experimentalSessionAndOrigin: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here if needed
+    },
+  },
+
+//projectId: "m4t5cs",
+  //projectId: "v4584h", 
+  e2e: {
+         //projectId: "m4t5cs",
+          setupNodeEvents(on, config) {
+            require('cypress-mochawesome-reporter/plugin')(on);
+      // node event listeners
+
+     
+    },
+   // baseUrl: 'https://comet.jainam.in/#/startup',
+  },
+});
+
