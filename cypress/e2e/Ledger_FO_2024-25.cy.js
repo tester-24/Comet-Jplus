46f5588cacd2d250296907a57f91d035721869aa
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-06-24 17:41:49 +0530

    Update Ledger_FO_2024-25.cy.js

--- cypress/e2e/Ledger_FO_2024-25.cy.js
+++ cypress/e2e/Ledger_FO_2024-25.cy.js
@@ -1,19 +1,19 @@
 describe('Ledger_FO_2024-25', () => {
     it('Ledger_FO_2024-25', () => {
        cy.Login()
-       cy.wait(4000)
+       cy.wait(1500)
   
      //  cy.origin('https://comet.jainam.in', () => {
-       cy.wait(8000)
+       //cy.wait(8000)
        
        // 1. Ledger All 2024-25
         cy.get("label:contains('Segment')").next("kendo-dropdownlist").find("button").click()
-        cy.wait(2000)
+        cy.wait(1500)
         cy.get('ul[role="listbox"]').contains('FUTURE_OPTION').click()
         cy.get("label:contains('FY Year')").next("kendo-dropdownlist").find("button").click()
-        cy.wait(2000)
+        cy.wait(1500)
         cy.contains('ul[role="listbox"] li', '2024-2025').click()
-        cy.wait(2000)
+        cy.wait(1500)
 
         //View Data
         cy.get("input[name='Code']").type("wa5320")
@@ -23,11 +23,11 @@ describe('Ledger_FO_2024-25', () => {
         cy.get("body > app-root:nth-child(1) > app-layout:nth-child(2) > div:nth-child(2) > app-ledger:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(2)").click()
         // Download PDF
         cy.get("#pdf").click()
-        cy.wait(2000)
+        cy.wait(5000)
         cy.get("body > app-root:nth-child(1) > app-layout:nth-child(2) > div:nth-child(2) > app-ledger:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(2)").click()
         // Close Popup
         cy.get("button[aria-label='Close']").click()
-        cy.wait(2000)
+        cy.wait(1500)
 
         
     }); 
